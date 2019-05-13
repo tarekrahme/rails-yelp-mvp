94ef516d93cce324e7c8b06b3f882a4a7641e29c
Tarek Rahme <rahmetarek@gmail.com>
2019-05-13 16:05:03 +0100

    Restaurants ap

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: %i[index show create new] do
+    resources :reviews, only: %i[index create new]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+restaurants_attributes = [
+  {
+    name: 'Fries',
+    address: '7 Brussel St, London E2 7JE',
+    category: 'belgian',
+    phone_number: '01205060'
+  },
+  {
+    name: 'Sushi',
+    address: '10 Tokyo St, London E1 9GD',
+    category: 'japanese',
+    phone_number: '02303030'
+  },
+  {
+    name: 'Pasta',
+    address: '70 Rome St, London Q3 9KI',
+    category: 'italian',
+    phone_number: '01225566'
+  },
+  {
+    name: 'Noodles',
+    address: '12 Beijing St, London A1 3RZ',
+    category: 'chinese',
+    phone_number: '01235363'
+  },
+  {
+    name: 'Pizza',
+    address: '43 Milan St, London T4 3YL',
+    category: 'italian',
+    phone_number: '01222222'
+  }
+]
+
+Restaurant.create!(restaurants_attributes)
